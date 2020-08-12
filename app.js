abd46e454ac0098878cd73d8c53c390a97a741fb
larprad <59915248+larprad@users.noreply.github.com>
2020-08-12 17:46:30 +0200

    Init side panel for grid config

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 ////////////// TETRIS //////////////
 
-// Game configuration
+// Game initial configuration
 
-const blockSize = 20;
-const rows = 20;
-const columns = 10;
+let blockSize = 15;
+let rows = 20;
+let columns = 10;
 
 ////////////////////////////////////
 
 // Game init scripts
-
+displayInitialConfiguration(blockSize, columns, rows);
 generatePlaygroundGrid(blockSize, columns, rows);
 
 ////////////////////////////////////
 
 // Game functions
+
+function displayInitialConfiguration(blockSize, columns, rows) {
+  document.getElementById('rowNumber').value = rows;
+  document.getElementById('columnNumber').value = columns;
+  document.getElementById('blockSize').value = blockSize;
+}
 
 // init playground divs
 function generatePlaygroundGrid(blocksWidth, playgroundColumns, playgroundRows) {
@@ -32,3 +38,34 @@ function generatePlaygroundGrid(blocksWidth, playgroundColumns, playgroundRows) 
   }
   console.log(`${numberOfBlocks} blocks have been generated`);
 }
+
+// delete all divs from the playground
+function cleanPlaygroundGrid() {
+  const playground = document.getElementById('playground');
+  while (playground.firstChild) {
+    playground.removeChild(playground.firstChild);
+  }
+}
+
+// updating global variables blockSize, rows & columns
+function updateGameConfiguration() {
+  const newRowNumber = document.getElementById('rowNumber');
+  const newColumnNumber = document.getElementById('columnNumber');
+  const newBlockSize = document.getElementById('blockSize');
+  rows = parseInt(newRowNumber.value, 10);
+  columns = parseInt(newColumnNumber.value, 10);
+  blockSize = parseInt(newBlockSize.value, 10);
+}
+
+// handling grid update from global variables blockSize, rows & columns
+function handleConfigUpdate() {
+  updateGameConfiguration();
+  cleanPlaygroundGrid();
+  generatePlaygroundGrid(blockSize, columns, rows);
+}
+
+////////////////////////////////////
+
+// Listener
+
+document.getElementById('configPanel').addEventListener('change', handleConfigUpdate);
